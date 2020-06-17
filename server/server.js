255463543ba27911f011ec1a41ddfea72c8b8b24
Mithun <shenoymithun70@gmail.com>
2020-06-17 17:25:00 +0530

    Connect & disconnect

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
+const { Socket } = require("dgram");
 
 const publicPath = path.join(__dirname, "/../public");
 const port = process.env.PORT || 3000;
-const app = express();
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("A new user just connected");
+  socket.on("disconnect", () => {
+    console.log("User was discconected from server");
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
