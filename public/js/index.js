91c390a904679750fd70c417c890f3eba0ae4b60
Mithun <shenoymithun70@gmail.com>
2020-06-17 19:02:48 +0530

    Added custom/broadcasting events

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+let socket = io();
+socket.on("connect", function () {
+  console.log("Connected to server");
+});
+socket.on("disconnect", function () {
+  console.log("Disconnected from server");
+});
+
+socket.on("newMessage", function (message) {
+  console.log("newMessage", message);
+});
